7f827b4d9ee426f9d56e55572f348d87a13e8867
yuno.k <120995031+Khang1z2t@users.noreply.github.com>
2024-11-07 15:43:24 +0700

    Add new Project

---
+++ src/main/java/com/tni/codezero/CodeZeroApplication.java
@@ -0,0 +1,13 @@
+package com.tni.codezero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeZeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeZeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tni/codezero/CodeZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tni.codezero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeZeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
